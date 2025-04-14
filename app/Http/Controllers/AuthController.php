0f92a542ec9edc0fd5eb0d4f7a3feb75ce91dce6
ryhazh <rayidevs@gmal.com>
2025-04-14 22:28:10 +0700

    gatau ah males

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
