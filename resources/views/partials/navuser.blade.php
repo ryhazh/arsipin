4d0b60043fda492852ca448b2250dfe201bcb66e
Ray <159104955+ryhazh@users.noreply.github.com>
2025-05-16 20:48:20 +0700

    Update navuser.blade.php

--- resources/views/partials/navuser.blade.php
+++ resources/views/partials/navuser.blade.php
@@ -1,16 +1,18 @@
-<div class="container py-2">
-    <div>
-        <ul class="nav nav-underline border-0">
-            <li class="nav-item">
-              <a class="nav-link {{ Request::is('dashbook') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook">Books</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link {{ Request::is('dashbook/requests') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook/requests">My Requests</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link {{ Request::is('dashbook/borrowed') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook/borrowed">My Borrowed Books</a>
-            </li>
-        </ul>          
+@if (!Request::is('login') && !Request::is('register'))
+    <div class="container py-2">
+        <div>
+            <ul class="nav nav-underline border-0">
+                <li class="nav-item">
+                  <a class="nav-link {{ Request::is('dashbook') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook">Books</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link {{ Request::is('dashbook/requests') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook/requests">My Requests</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link {{ Request::is('dashbook/borrowed') ? 'active' : '' }} px-3 pb-2 mb-n1" href="/dashbook/borrowed">My Borrowed Books</a>
+                </li>
+            </ul>          
+        </div>
     </div>
-</div>
-<hr class="m-0">
+    <hr class="m-0">
+@endif
